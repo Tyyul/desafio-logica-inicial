1f10578cb6c6b44bbb11b68e2cda59b5e2524b42
Tyyul <tyyul.ka@gmail.com>
2024-10-29 02:58:13 -0300

    Terceiro commit

--- desafio-do-felipao/index.js
+++ desafio-do-felipao/index.js
@@ -1,12 +1,6 @@
-//Variáveis
-
-let nomeHeroi = ""
-let nivelHeroi = ""
-
 // Coletar entrada do usuário usando prompt
 let nomeHeroi = prompt("Informe o nome do Herói:");
+let nivelHeroi = prompt("Informe o nível do Herói:");
 
-// Coletar nível do herói
-nivelHeroi = prompt("Informe o nível do Herói:");
 
 console.log("O Herói: " + nomeHeroi + ", Nível: " + nivelHeroi);
